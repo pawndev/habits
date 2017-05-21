aaf14705b8d538c74ef464705587a1af346027ae
Christophe Coquelet <coquelet.c@gmail.com>
2017-05-21 21:31:24 +0200

    example controller

---
+++ app/Modules/Common/Controller/IndexController.ts
@@ -0,0 +1,42 @@
+import * as Hapi from 'hapi';
+import { controller, get, post, put, cache, config, route, validate, Controller } from 'hapi-decorators';
+
+@controller('/test')
+export default class TestController implements Controller {
+    baseUrl: string;
+    routes: () => Hapi.RouteConfiguration[];
+
+    @get('/')
+    @config({
+        auth: false
+    })
+    @cache({
+        expiresIn: 42000
+    })
+    @validate({
+        payload: false
+    })
+    getHandler(request: Hapi.Request, reply: Hapi.IReply) {
+        reply({ success: true, msg: "billy" });
+    }
+
+    @post('/')
+    postHandler(request: Hapi.Request, reply: Hapi.IReply) {
+        reply({ success: true });
+    }
+
+    @put('/{id}')
+    putHandler(request: Hapi.Request, reply: Hapi.IReply) {
+        reply({ success: true });
+    }
+
+    @route('delete', '/{id}')
+    deleteHandler(request: Hapi.Request, reply: Hapi.IReply) {
+        reply({ success: true });
+    }
+
+    @get('/{id}')
+    getOneHandler(request: Hapi.Request, reply: Hapi.IReply) {
+        reply({ success: true, param: request.params.id });
+    }
+}
