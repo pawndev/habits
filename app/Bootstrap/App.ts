9137e8892069c8289329b1b6da75177850f714f1
Christophe Coquelet <coquelet.c@gmail.com>
2017-05-21 21:54:13 +0200

    mock AddRoutes method

--- app/Bootstrap/App.ts
+++ app/Bootstrap/App.ts
@@ -3,6 +3,7 @@ import { IPlugin } from '../Modules/Common/Interface/Hapi/IPlugin';
 
 export default class App {
     private server: Hapi.Server;
+    private baseModulePath: string = `../Modules`;
 
     constructor(HapiSettings: Object) {
         this.server = new Hapi.Server();
@@ -13,17 +14,40 @@ export default class App {
         return this.server.start();
     }
 
-    AddDecorations(decorationsArray: Array<IPlugin>) {
+    AddModules(ModulesObject) {
+
+        for (let module in ModulesObject) {
+            let currentController = require(`${this.baseModulePath}/Controller/`)
+        }
+
+        return this;
+    }
+
+    private AddDecorations(decorationsArray: Array<IPlugin>) {
         this.server.register(decorationsArray);
+        
         return this;
     }
 
-    AddRoutes(routesConfig: Array<Hapi.RouteConfiguration>) {
-        this.server.route(routesConfig);
+    private AddRoutes(ControllersArray: string[]) {
+        let indexControllerArray = 0;
+        let controllerArrayLength = ControllersArray.length;
+        let controllersInstance: Array<Hapi.RouteConfiguration> = [];
+
+        for (;indexControllerArray < controllerArrayLength; indexControllerArray++) {
+            let currentControllerString: string = ControllersArray[indexControllerArray];
+            let currentControllerImport = require(`${this.baseModulePath}/Controller/${currentControllerString}`);
+            let currentController = new currentControllerImport();
+
+            controllersInstance.push(currentController);
+        }
+        
+        this.server.route(controllersInstance);
+
         return this;
     }
 
-    AddPlugins(pluginsArray: Array<IPlugin>) {
+    private AddPlugins(pluginsArray: Array<IPlugin>) {
         this.server.register(pluginsArray);
         return this;
     }
